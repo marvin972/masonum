dc29fdb419cee100d4d350b7e908cecbdd50cd0b
marvin972 <105202057+marvin972@users.noreply.github.com>
2022-10-28 16:08:21 +0200

    Mis en place du ReCaptcha V3
    Page contact fini

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -8,6 +8,8 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Karser\Recaptcha3Bundle\Form\Recaptcha3Type;
+use Karser\Recaptcha3Bundle\Validator\Constraints\Recaptcha3;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -77,8 +79,13 @@ class ContactType extends AbstractType
                     'class' => 'btn btn-danger mt-4'
                 ],
                 'label' => 'Envoyer'
+            ])
+            // Recaptchav3 de google 
+            ->add('captcha', Recaptcha3Type::class, [
+                'constraints' => new Recaptcha3(),
+                'action_name' => 'contact'
             ]);
-        ;
+        
     }
 
     public function configureOptions(OptionsResolver $resolver): void
